ddd900483d9e86424a463bfef26287d57111f80c
Renzo Borgatti <reborg@reborg.net>
2013-09-20 15:30:21 +0100

    0.2.1 upgrade

--- pwc.rb
+++ pwc.rb
@@ -3,8 +3,8 @@ require 'formula'
 class Pwc < Formula
 
   homepage 'https://github.com/reborg/pwc'
-  url 'https://github.com/reborg/pwc/archive/0.1.1.tar.gz'
-  sha1 'becfeba8fece1ecdd0a3290993a0fbe245a48a7a'
+  url 'https://github.com/reborg/pwc/archive/0.2.1.tar.gz'
+  sha1 '2ee0b22b70146a8486edb1b7fe69746f71143b69'
 
   head "https://github.com/reborg/pwc.git"
 
