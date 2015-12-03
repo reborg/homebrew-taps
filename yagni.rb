3f4cb9956d7f5a00d5f22f227fefe5891601a218
reborg <reborg@reborg.net>
2015-12-03 11:42:36 +0000

    again wrong sha

--- yagni.rb
+++ yagni.rb
@@ -4,7 +4,7 @@ class Yagni < Formula
 
   homepage 'https://github.com/reborg/yagni-detector'
   url 'https://github.com/reborg/yagni-detector/archive/0.0.1.tar.gz'
-  sha1 '3d65b84e33116be14a801a39134c4ebcf9fa6e18'
+  sha1 '90aec65adbff1d06e8434dd1a7f9cb2a07568d12'
 
   head "https://github.com/reborg/yagni-detector.git"
 
